4884ae68654d3f86fff1f5917fde558ac2ab90b4
Khem <khemkandel@gmail.com>
2025-05-12 17:47:20 -0500

    Final Cleanup

--- econ8320-semester-project-dashboard.py
+++ econ8320-semester-project-dashboard.py
@@ -178,7 +178,7 @@ with st.sidebar:
     selected = option_menu(
     menu_title = "Hope Foundation",
     options = ["Overview","Last Year - Overview", "Request Status", "Funds Distributions","Demographics", "Data Quality"],
-    icons = ["house","rewind","activity","Population","	Validation/Test"],
+    icons = ["house","rewind","activity","Notepad","Population","Validation/Test"],
     menu_icon = "cast",
     default_index = 0,
     #orientation = "horizontal",
