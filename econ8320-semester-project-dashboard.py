747148e9c24a3fe535c2287b6317fb9716cebd31
Khem <khemkandel@gmail.com>
2025-05-12 21:54:58 -0500

    Final Cleanup

--- econ8320-semester-project-dashboard.py
+++ econ8320-semester-project-dashboard.py
@@ -547,8 +547,8 @@ elif selected == "Funds Distributions":
             #if show_by_pay_dur:
                 df_columns = ['Race','Gender','Insurance Type','Grant Req Date','Payment Date','Year']
                 df = subset_df(df=data_c,column='Payment Date',condition='', op='notna')[df_columns] 
+                df = df[df['Year'] == selected_year]
                 st.dataframe(df)
-                # df = df[df['Year'] == selected_year]
                 # df['DaysTillPaid']  =  (df['Payment Date'] - df['Grant Req Date']).dt.days
                 # df_columns =  ['Race','Gender','Insurance Type','DaysTillPaid']
                 # df = df[df_columns]
