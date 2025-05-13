83dbbf495f7bf78c8070de2493d9b8a13790af4c
Khem <khemkandel@gmail.com>
2025-05-12 21:53:40 -0500

    Final Cleanup

--- econ8320-semester-project-dashboard.py
+++ econ8320-semester-project-dashboard.py
@@ -547,12 +547,13 @@ elif selected == "Funds Distributions":
             #if show_by_pay_dur:
                 df_columns = ['Race','Gender','Insurance Type','Grant Req Date','Payment Date','Year']
                 df = subset_df(df=data_c,column='Payment Date',condition='', op='notna')[df_columns] 
-                df = df[df['Year'] == selected_year]
-                df['DaysTillPaid']  =  (df['Payment Date'] - df['Grant Req Date']).dt.days
-                df_columns =  ['Race','Gender','Insurance Type','DaysTillPaid']
-                df = df[df_columns]
-                df_filtered_demography = df[df['DaysTillPaid'] >= 0].groupby(df_columns)['DaysTillPaid'].count().sort_values(ascending=False).reset_index(name='Total Records')
-                st.dataframe(df_filtered_demography)
+                st.dataframe(df)
+                # df = df[df['Year'] == selected_year]
+                # df['DaysTillPaid']  =  (df['Payment Date'] - df['Grant Req Date']).dt.days
+                # df_columns =  ['Race','Gender','Insurance Type','DaysTillPaid']
+                # df = df[df_columns]
+                # df_filtered_demography = df[df['DaysTillPaid'] >= 0].groupby(df_columns)['DaysTillPaid'].count().sort_values(ascending=False).reset_index(name='Total Records')
+                # st.dataframe(df_filtered_demography)
 
 
     c3 = st.container()
